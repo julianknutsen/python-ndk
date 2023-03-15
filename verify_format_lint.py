529673090efc8c139057eea43f9d41e2ef7714ce
Julian Knutsen <julianknutsen@users.noreply.github.com>
2023-03-14 23:55:59 -0700

    [style] Fix the lint errors

--- verify_format_lint.py
+++ verify_format_lint.py
@@ -19,25 +19,34 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 
+import os
 import subprocess
 import sys
 
-# run each tool and capture the output to stdout/stderr
-# if any tool fails, set a flag and print an error message
-# then exit with a non-zero exit code
-failed = False
 
 def _run_cmd(args: list[str]) -> bool:
-    print(f'Running {args} ...')
+    print(f"Running {args} ...")
     output = subprocess.run(args, capture_output=True, text=True, check=False)
     print(output.stdout)
     print(output.stderr)
     return bool(output.returncode)
 
 
-sys.exit(any([
-    _run_cmd(['black', '--check', '.']),
-    _run_cmd(['isort', '--check-only', '.']),
-    _run_cmd(['pylint', 'functional_tests/', 'ndk', 'cli']),
-    _run_cmd(['mypy', '.'])
-]))
+py_files = []
+for root, dirs, files in os.walk("."):
+    for file in files:
+        if "venv" in root:
+            continue
+        if file.endswith(".py"):
+            py_files.append(os.path.join(root, file))
+
+sys.exit(
+    any(
+        [
+            _run_cmd(["black", "--check", "."]),
+            _run_cmd(["isort", "--check-only", "."]),
+            _run_cmd(["pylint"] + py_files),
+            _run_cmd(["mypy", "."]),
+        ]
+    )
+)
